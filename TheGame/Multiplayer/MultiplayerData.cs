01c9de1ec6ac50714bce71861ef10674486a3598
Michał Matusiak <218156@edu.p.lodz.pl>
2023-04-15 13:12:17 +0200

    multiplayer intro

---
+++ TheGame/Multiplayer/MultiplayerData.cs
@@ -0,0 +1,8 @@
+﻿namespace TheGame.Multiplayer
+{
+    internal class MultiplayerData
+    {
+        public string username { get; set; }
+        public string userPrivateKey { get; set; }
+    }
+}
---
+++ TheGame/Multiplayer/MultiplayerUserConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace TheGame.Multiplayer
+{
+    public class MultiplayerUserConfig
+    {
+        MultiplayerData data;
+        private string path = Environment.ExpandEnvironmentVariables("%userprofile%/documents/TheGame");
+
+
+        public bool CheckIfUserIsConfigured()
+        {
+            if (File.Exists(path + "/mulpiplayerConfig.xml"))
+            {
+                try
+                {
+                    //login
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void SaveUserConfiguration(string username)
+        {
+            //ask API for userPrivateKey
+            data.userPrivateKey = "";
+            data.username = username;
+
+            if (!(File.Exists(path)))
+                Directory.CreateDirectory(path);
+            using (var stream = new FileStream(path + "/mulpiplayerConfig.xml", FileMode.Create))
+            {
+                var XML = new XmlSerializer(typeof(MultiplayerData));
+                XML.Serialize(stream, data);
+            }
+
+        }
+    }
+}
